0e1a92ac3796fcfebf6a20146744ad4abb6778e0
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2024-07-13 02:44:13 +0300

    implement global exception handler

---
+++ src/main/java/com/github/yildizmy/exception/GlobalExceptionHandler.java
@@ -0,0 +1,89 @@
+package com.github.yildizmy.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Objects;
+
+import static com.github.yildizmy.common.Constants.*;
+
+@Slf4j(topic = "GlobalExceptionHandler")
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // this parameter is used to print StackTrace while debugging (its value is false by default)
+    @Value("${exception.trace:false}")
+    private boolean printStackTrace;
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleAccountNotFoundException(AccountNotFoundException ex, WebRequest request) {
+        log.error(REQUESTED_ACCOUNT_NOT_FOUND);
+        return buildErrorResponse(ex, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    @ResponseStatus(HttpStatus.PRECONDITION_FAILED)
+    public ResponseEntity<Object> handleInsufficientBalanceException(InsufficientBalanceException ex, WebRequest request) {
+        log.error(INSUFFICIENT_BALANCE_EXCEPTION);
+        return buildErrorResponse(ex, HttpStatus.PRECONDITION_FAILED, request);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleIllegalStateException(IllegalStateException ex, WebRequest request) {
+        log.error(ILLEGAL_STATE_EXCEPTION);
+        return buildErrorResponse(ex, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAllUncaughtException(Exception ex, WebRequest request) {
+        log.error(UNKNOWN_ERROR, ex);
+        return buildErrorResponse(ex, UNKNOWN_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception ex,
+                                                      HttpStatusCode statusCode,
+                                                      WebRequest request) {
+        return buildErrorResponse(ex, ex.getMessage(), statusCode, request);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception ex,
+                                                      String message,
+                                                      HttpStatusCode statusCode,
+                                                      WebRequest request) {
+        final ErrorResponse errorResponse = new ErrorResponse(statusCode.value(), message);
+        if (printStackTrace && isTraceOn(request)) {
+            errorResponse.setStackTrace(ExceptionUtils.getStackTrace(ex));
+        }
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    private boolean isTraceOn(WebRequest request) {
+        String[] value = request.getParameterValues(TRACE);
+        return Objects.nonNull(value)
+                && value.length > 0
+                && value[0].contentEquals("true");
+    }
+
+    @Override
+    public ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatusCode statusCode,
+            WebRequest request) {
+        return buildErrorResponse(ex, statusCode, request);
+    }
+}
