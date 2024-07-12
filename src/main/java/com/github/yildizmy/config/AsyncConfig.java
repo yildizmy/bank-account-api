4d152ed41fc4cdae722379729855a25fb8a78769
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2024-07-13 02:47:32 +0300

    implement async config

---
+++ src/main/java/com/github/yildizmy/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.github.yildizmy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    // configure async settings here if needed
+}
