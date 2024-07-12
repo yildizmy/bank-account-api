937cf68921dd342db7b64848b8c1ce84c9846815
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2024-07-13 02:41:39 +0300

    implement bank account repository

---
+++ src/main/java/com/github/yildizmy/repository/BankAccountRepository.java
@@ -0,0 +1,11 @@
+package com.github.yildizmy.repository;
+
+import com.github.yildizmy.entity.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+}
