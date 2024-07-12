51f67b7bf034b885bf9690bd90cffd1a6d8a48ae
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2024-07-13 02:45:12 +0300

    implement h2 config

---
+++ src/test/java/com/github/yildizmy/BankAccountApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.yildizmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
