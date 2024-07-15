152e86646d48b2da709462c28e47a2be4df55160
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2024-07-15 12:22:56 +0300

    apply validation for money amount requests

--- src/main/java/com/github/yildizmy/dto/request/MoneyAmountRequest.java
+++ src/main/java/com/github/yildizmy/dto/request/MoneyAmountRequest.java
@@ -1,10 +1,15 @@
 package com.github.yildizmy.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class MoneyAmountRequest {
+
+    @Min(value = 1, message = "{amount.min}")
+    @NotNull(message = "{amount.notNull}")
     private BigDecimal amount;
 }
