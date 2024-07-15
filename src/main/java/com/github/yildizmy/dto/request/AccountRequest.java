3af0c8d5a299786c31b028dbe6f98b62534312fc
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2024-07-15 12:22:37 +0300

    apply validation for account requests

--- src/main/java/com/github/yildizmy/dto/request/AccountRequest.java
+++ src/main/java/com/github/yildizmy/dto/request/AccountRequest.java
@@ -1,11 +1,19 @@
 package com.github.yildizmy.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 import java.math.BigDecimal;
 
 @Value
 public class AccountRequest {
+
+    @Min(value = 1, message = "{initialBalance.min}")
+    @NotNull(message = "{initialBalance.notNull}")
     private BigDecimal initialBalance;
+
+    @NotBlank(message = "{owner.notBlank}")
     private String owner;
 }
